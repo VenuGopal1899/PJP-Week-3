5f5e216896676170a0410c63dc8571c33f243146
VenuGopal1899 <venumalladi18@gmail.com>
2021-03-25 13:04:26 +0530

    findTranslate method added

--- DateTimeCalculator.java
+++ DateTimeCalculator.java
@@ -9,13 +9,15 @@ class DateTimeCalculator {
 
 		DateTimeCalc d = new DateTimeCalc();
 
-		String dt = "20/03/2021";
+		String dt = "02/03/2020";
 		String dt2 = "29/03/2021";
 
 		d.addDate(dt, 2);
 		d.subDate(dt, dt2);
 		d.findDayWeek(dt);
 		d.findWeekDay(dt);
+		d.findTranslate(dt, "yesterday", 0);
+		d.findTranslate(dt, "Month Before", 0);
 
 		System.out.println("--------");
 		System.out.println(d.ndate);
@@ -28,6 +30,8 @@ class DateTimeCalculator {
 		System.out.println(d.dayOfWeek);
 		System.out.println("--------");
 		System.out.println(d.weekOfDay);
+		System.out.println("--------");
+		System.out.println(d.ndate1);
 	}
 }
 
@@ -40,7 +44,7 @@ class DateTimeCalc{
 	public String ndate;
 	public String weekname;
 	public int dayOfWeek;
-	private String ndate1;
+	public String ndate1;
 
 	public void addDate(String date1, int day) {
 		DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
@@ -64,9 +68,7 @@ class DateTimeCalc{
 		LocalDate localdt1 = LocalDate.parse(date1, df);
 
 		this.weekname = localdt1.getDayOfWeek().name();
-
-		int weekNumber = localdt1.getDayOfWeek().getValue();
-		this.dayOfWeek = (weekNumber == 7) ? 0 : weekNumber;
+		this.dayOfWeek = (localdt1.getDayOfWeek().getValue())%7;
 	}
 
 	public void findWeekDay(String date1) {
@@ -77,19 +79,88 @@ class DateTimeCalc{
 		this.weekOfDay = localdt1.get(weekFields.weekOfWeekBasedYear());
 	}
 
-	// public void findTranslate(String date1, String phrases, int n) {
+	public void findTranslate(String date1, String phrases, int n) {
 
-	// 	// find the new date by using the phrases and store in ndate1
-	// 	// Translate natural language phrases like
-	// 	// *Today, Tomorrow, Day-after-tomorrow,
-	// 	// yesterday, Day-before-yesterday, Last week, Previous week, Next week,
-	// 	// Next month, Next Year,  Last month,
-	// 	// Last Year, Month after, Month before,
-	// 	// n weeks from now, n days from now,
-	// 	// n months from now, n years from now,
-	// 	// n weeks earlier, n days earlier,
-	// 	// n months earlier, n years earlier*
+		/*
+		Find the new date by using the phrases and store in ndate1
+		Translate natural language phrases like
+		Today, Tomorrow, Day-after-tomorrow, yesterday, Day-before-yesterday,
+		Last week, Previous week, Next week, Next month, Next Year,  Last month,
+		Last Year, Month after, Month before, n weeks from now, n days from now,
+		n months from now, n years from now, n weeks earlier, n days earlier,
+		months earlier, n years earlier
+		*/
 
-	// }
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate localdt = LocalDate.parse(date1, df);
+
+		String givenPhrase = phrases.toLowerCase();
+
+		switch(givenPhrase) {
+			case "today":
+				this.ndate1 =  df.format(localdt.plusDays(0));
+				break;
+			case "tomorrow":
+				this.ndate1 =  df.format(localdt.plusDays(1));
+				break;
+			case "day-after-tomorrow":
+				this.ndate1 =  df.format(localdt.plusDays(2));
+				break;
+			case "yesterday":
+				this.ndate1 =  df.format(localdt.minusDays(1));
+				break;
+			case "previous week":
+				this.ndate1 =  df.format(localdt.minusDays(7));
+				break;
+			case "next week":
+				this.ndate1 =  df.format(localdt.plusDays(7));
+				break;
+			case "month after":
+			case "next month":
+				this.ndate1 =  df.format(localdt.plusMonths(1));
+				break;
+			case "next year":
+				this.ndate1 =  df.format(localdt.plusYears(1));
+				break;
+			case "last week":
+				this.ndate1 =  df.format(localdt.minusDays(7));
+				break;
+			case "month before":
+			case "last month":
+				this.ndate1 =  df.format(localdt.minusMonths(1));
+				break;
+			case "last year":
+				this.ndate1 =  df.format(localdt.minusYears(1));
+				break;
+			case "n weeks from now":
+				this.ndate1 =  df.format(localdt.plusDays(7*n));
+				break;
+			case "n days from now":
+				this.ndate1 =  df.format(localdt.plusDays(n));
+				break;
+			case "n months from now":
+				this.ndate1 =  df.format(localdt.plusMonths(n));
+				break;
+			case "n years from now":
+				this.ndate1 =  df.format(localdt.plusYears(n));
+				break;
+			case "n weeks earlier":
+				this.ndate1 =  df.format(localdt.minusDays(7*n));
+				break;
+			case "n days earlier":
+				this.ndate1 =  df.format(localdt.minusDays(n));
+				break;
+			case "n months earlier":
+				this.ndate1 =  df.format(localdt.minusMonths(n));
+				break;
+			case "n years earlier":
+				this.ndate1 =  df.format(localdt.minusYears(n));
+				break;
+			default:
+				this.ndate1 = "";
+				break;
+		}
+
+	}
 
 }
